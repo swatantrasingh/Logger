4aa6adf3823eed828c7cef36df86b33f9390f0ff
swatantra <swatantra.singh@koovs.com>
2021-08-24 21:07:28 +0530

    adssadasd

---
+++ Logger/Logger/Classes/LoggData.swift
@@ -0,0 +1,17 @@
+//
+//  LoggData.swift
+//  Logger
+//
+//  Created by Swatantra on 24/08/21.
+//
+
+import Foundation
+
+public class Logger {
+    public var name:String
+    
+    public init(s:String){
+        self.name=s
+    }
+    
+}
